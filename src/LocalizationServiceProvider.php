691dca4bb0f29c0ed029e11e4f1d8d7389ffe36f
Matteo Palazzo <matteo.palazzo@insology.com>
2025-06-23 14:56:10 +0200

    Adds lang() macro

--- src/LocalizationServiceProvider.php
+++ src/LocalizationServiceProvider.php
@@ -2,7 +2,22 @@
 
 namespace Goodcat\I10n;
 
-class LocalizationServiceProvider
+use Illuminate\Routing\Route;
+use Illuminate\Support\ServiceProvider;
+
+class LocalizationServiceProvider extends ServiceProvider
 {
-    //
+    public function boot(): void
+    {
+        //
+    }
+    public function register(): void
+    {
+        Route::macro('lang', function (array $translations = []) {
+            /** @var Route $this */
+            $this->action['localized_path'] = $translations;
+
+            return $this;
+        });
+    }
 }
