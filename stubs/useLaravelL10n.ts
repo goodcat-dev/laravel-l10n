2767097b8df079936a8a60cdd45fa4feebf705c0
Matteo <outlaw.plz@gmail.com>
2026-01-11 00:04:32 +0100

    Adds useLaravelL10n composable

---
+++ stubs/useLaravelL10n.ts
@@ -0,0 +1,22 @@
+import { route as ziggy } from '../../vendor/tightenco/ziggy';
+import { useI18n } from 'vue-i18n';
+
+type Params = Record<string, unknown> & { lang?: string };
+
+export function useLaravelL10n() {
+    const { locale } = useI18n();
+
+    const route = (name: string, params?: Params, absolute = true): string => {
+        const localized = name.concat('.', params?.lang ?? locale.value);
+
+        delete params?.lang;
+
+        if (ziggy().has(localized)) {
+            name = localized;
+        }
+
+        return ziggy(name, params, absolute);
+    };
+
+    return { route };
+}
